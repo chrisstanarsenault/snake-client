a17a0829e459abc7aa1dfe670ce86971c3c3be79
Chris Arsenault <chrisstanarsenault@gmail.com>
2019-10-27 00:58:13 -0400

    added connect function into a module within client.js

---
+++ client.js
@@ -0,0 +1,20 @@
+const net = require('net');
+
+const connect = () => {
+
+  const conn = net.createConnection({
+    host: 'localhost',
+    port: 50541
+  });
+
+  conn.on('data', (data) => {
+    console.log(data)
+  })
+
+  // interpret incoming data as text
+  conn.setEncoding('utf8');
+
+  return conn;
+}
+
+ module.exports = { connect };
